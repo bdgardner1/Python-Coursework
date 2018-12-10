27c784d81e32cd6f251765a1e0e82b91dbaf050d
bdgardner1 <43339667+bdgardner1@users.noreply.github.com>
2018-12-10 11:36:53 +0000

    change

--- agentframework.py
+++ agentframework.py
@@ -1,14 +1,14 @@
 import random
-class sheep_dog():
-    def __init__ (self, agents, num_of_agents):
+class sheep_dog(): 
+    def __init__ (self, agents, num_of_agents): #a sheepdog is created at the centre of the field
         self.x = 50
         self.y = 50
         self.agents = agents
-        self.store = 500
+        self.store = 500 # The sheepdog has '500' energy at the start
         self.num_of_agents = num_of_agents
     def move(self):
-        if self.store > 20:
-            if random.random() < 0.5:
+        if self.store > 20: #moving depletes the sheepdogs energy by 20
+            if random.random() < 0.5: # sheepdog moves randomly
                 self.x = (self.x + 3) % 100
             else:
                self.x = (self.x - 3) % 100
@@ -19,28 +19,28 @@ class sheep_dog():
             self.store -= 3
     def herd_sheep(self,num_of_agents):
         for agent in self.agents:
-            dist = self.distancetosheep(agent)
+            dist = self.distancetosheep(agent) # calculates how far the sheepdog is from the sheep
             print (dist)
-            if dist < 10:
+            if dist < 10: #if within a certain distance of the sheep, the sheep returns to the centre
                 agent.x = 50
                 agent.y = 50
                 self.num_of_agents -= 1
-                self.store += 250
+                self.store += 250 #sheepdog gains energy (treat from farmer)
                 
     def distancetosheep(self, agent):
         return (((self.x - agent.x)**2) + ((self.y - agent.y)**2))**0.5
     
         
 class Agent():
-    def __init__(self, environment, agents):
+    def __init__(self, environment, agents): # sheep are created with random location
         self.x = random.randint(0,99)
         self.y = random.randint(0,99)
-        self.environment = environment
+        self.environment = environment 
         self.agents = agents
-        self.store = 0 # We'll come to this in a second.
+        self.store = 0 
     
                 
-    def move(self):
+    def move(self): # Sheep move randomly twice
         if random.random() < 0.5:
             self.x = (self.x + 1) % 100
         else:
@@ -50,14 +50,14 @@ class Agent():
         else:
            self.y = (self.y - 1) % 100
 
-    def eat(self): # can you make it eat what is left?
+    def eat(self): # Sheep either eat 10 units or whatever is left
         if self.environment[self.y][self.x] > 10:
             self.environment[self.y][self.x] -= 10
             self.store += 10
         else:
             self.store = self.store + self.environment[self.y][self.x]
             self.environment[self.y][self.x] = 0
-    def share_with_neighbours(self,neighbourhood):
+    def share_with_neighbours(self,neighbourhood): # Sheep share with other sheep within a certain distance
        for agent in self.agents:
         dist = self.distance_between(agent) 
         if dist <= neighbourhood:
